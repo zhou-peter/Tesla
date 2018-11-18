de739a6aaac6bfc2484f0b5e7c9617cf29f501e4
AlexeySinushkin <sinushkin_alexey@mail.ru>
2018-11-18 16:53:18 +0800

    Consol закрывается с кнопки Х

--- Control/TeslaCommunication/service.cs
+++ Control/TeslaCommunication/service.cs
@@ -3,78 +3,218 @@
 
 using System;
 using System.Configuration;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
 using System.ServiceModel;
+using System.Threading;
+using System.Windows;
 
-namespace Microsoft.ServiceModel.Samples
+namespace TeslaCommunication
 {
     // Define a service contract.
-    [ServiceContract(Namespace="http://Microsoft.ServiceModel.Samples")]
-    public interface ICalculator
+    [ServiceContract(Namespace = "http://STM32TeslaCommunication")]
+    public interface ICommunicationProtocol
     {
         [OperationContract]
-        double Add(double n1, double n2);
-        [OperationContract]
-        double Subtract(double n1, double n2);
-        [OperationContract]
-        double Multiply(double n1, double n2);
-        [OperationContract]
-        double Divide(double n1, double n2);
+        bool Connect(string comPortName);
+
     }
 
-    // Service class which implements the service contract.
-    // Added code to write output to the console window
-    public class CalculatorService : ICalculator
+    class TeslaServiceHost : ServiceHost
     {
-        public double Add(double n1, double n2)
+        public TeslaServiceHost():
+            base(Service.getInstance())
         {
-            double result = n1 + n2;
-            Console.WriteLine("Received Add({0},{1})", n1, n2);
-            Console.WriteLine("Return: {0}", result);
-            return result;
+            //thread = new Thread(new ThreadStart(()=> { Open(); }));
+            
         }
 
-        public double Subtract(double n1, double n2)
+        //public Thread thread;
+
+        protected override void OnAbort()
         {
-            double result = n1 - n2;
-            Console.WriteLine("Received Subtract({0},{1})", n1, n2);
-            Console.WriteLine("Return: {0}", result);
-            return result;
+            base.OnAbort();
+            Service.getInstance().OnClose();
         }
-
-        public double Multiply(double n1, double n2)
+        protected override void OnClose(TimeSpan timeout)
         {
-            double result = n1 * n2;
-            Console.WriteLine("Received Multiply({0},{1})", n1, n2);
-            Console.WriteLine("Return: {0}", result);
-            return result;
+            base.OnClose(timeout);
+            Service.getInstance().OnClose();
         }
-
-        public double Divide(double n1, double n2)
-        {
-            double result = n1 / n2;
-            Console.WriteLine("Received Divide({0},{1})", n1, n2);
-            Console.WriteLine("Return: {0}", result);
-            return result;
-        }
+    }
 
 
+
+    /*InstanceContextMode необходимо установить на InstanceContextMode.Single. 
+     * Это можно осуществить через атрибут ServiceBehaviorAttribute*/
+    [ServiceBehaviorAttribute(InstanceContextMode =InstanceContextMode.Single)]
+    public class Service : ICommunicationProtocol
+    {
+
+        static ServiceHost serviceHost;
         // Host the service within this EXE console application.
         public static void Main()
         {
             // Create a ServiceHost for the CalculatorService type.
-            using (ServiceHost serviceHost = new ServiceHost(typeof(CalculatorService)))
+            using (serviceHost = new TeslaServiceHost())
             {
                 // Open the ServiceHost to create listeners and start listening for messages.
                 serviceHost.Open();
 
                 // The service can now be accessed.
                 Console.WriteLine("The service is ready.");
-                Console.WriteLine("Press <ENTER> to terminate service.");
+                SetConsoleCtrlHandler(new HandlerRoutine(ConsoleCtrlCheck), true);
+                Console.WriteLine("CTRL+C,CTRL+BREAK or suppress the application to exit");
                 Console.WriteLine();
                 Console.ReadLine();
+                // Close the ServiceHost.
+                serviceHost.Close();
 
             }
+
+
         }
+
+#region Остановка с кнопки Х
+
+        private static bool ConsoleCtrlCheck(CtrlTypes ctrlType)
+        {
+            // Put your own handler here
+            Service.getInstance().OnClose();
+            switch (ctrlType)
+            {
+                case CtrlTypes.CTRL_CLOSE_EVENT:
+                    isclosing = true;
+                    Console.WriteLine("Program being closed!");
+                    break;
+            }
+
+            return true;
+        }
+
+        
+
+
+        private static bool isclosing = false;
+        // Declare the SetConsoleCtrlHandler function
+        // as external and receiving a delegate.
+
+        [DllImport("Kernel32")]
+        public static extern bool SetConsoleCtrlHandler(HandlerRoutine Handler, bool Add);
+
+        // A delegate type to be used as the handler routine
+        // for SetConsoleCtrlHandler.
+        public delegate bool HandlerRoutine(CtrlTypes CtrlType);
+
+        // An enumerated type for the control messages
+        // sent to the handler routine.
+
+        public enum CtrlTypes
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT,
+            CTRL_CLOSE_EVENT,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+        private Service() { }
+        static Service instance;
+        public static Service getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Service();
+            }
+            return instance;
+        }
+
+
+        SerialPort sp = null;
+        bool connectionOpen = false;
+
+
+        public void OnClose() { 
+            if (sp != null && sp.IsOpen)
+            {
+                sp.BaseStream.Flush();
+                sp.Close();
+            }
+        }
+
+
+        public bool Connect(string comPortName)
+        {
+            //If we are not connected, connect
+            if (connectionOpen == false)
+            {
+                try
+                {
+                    sp = new SerialPort(comPortName);
+                    sp.BaudRate = 9600;
+                    sp.Parity = Parity.None;
+                    sp.DataBits = 8;
+                    sp.Open();
+                    //sp.DtrEnable = true;
+                    sp.BaseStream.Flush();
+                    sp.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);
+
+                    connectionOpen = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    return false;
+                }
+            }
+            else//disconnect
+            {
+                try
+                {
+                    if (sp != null && sp.IsOpen)
+                    {
+                        sp.BaseStream.Flush();
+                        sp.Close();
+                    }
+                    connectionOpen = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                int length = sp.BytesToRead;
+                byte[] buf = new byte[length];
+                sp.Read(buf, 0, length);
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "On receive");
+            }
+        }
+
+
 
     }
 
