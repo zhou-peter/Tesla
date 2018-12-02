6ec83db63adb5cd71ad22bc8f72be468126d1747
AlexeySinushkin <sinushkin_alexey@mail.ru>
2018-12-02 15:38:19 +0800

    two projects debug at a time

---
+++ Control/TeslaDesktopClient/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TeslaDesktopClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxCom = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelLed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxCom
+            // 
+            this.textBoxCom.Location = new System.Drawing.Point(12, 12);
+            this.textBoxCom.Name = "textBoxCom";
+            this.textBoxCom.Size = new System.Drawing.Size(84, 20);
+            this.textBoxCom.TabIndex = 0;
+            this.textBoxCom.Text = "Com2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Подключиться";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelLed
+            // 
+            this.labelLed.AutoSize = true;
+            this.labelLed.Location = new System.Drawing.Point(776, 15);
+            this.labelLed.Name = "labelLed";
+            this.labelLed.Size = new System.Drawing.Size(22, 13);
+            this.labelLed.TabIndex = 2;
+            this.labelLed.Text = "[   ]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelLed);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxCom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxCom;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelLed;
+    }
+}
+
---
+++ Control/TeslaDesktopClient/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeslaDesktopClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            client=new TeslaCommunication.CommunicationProtocolClient();
+            client.Open();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            client.Close();
+        }
+
+        TeslaCommunication.CommunicationProtocolClient client;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.Connect(textBoxCom.Text);
+        }
+    }
+}
