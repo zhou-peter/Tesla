b3597071597a2e2ee305ecdc583e08b673f1b342
Alexey <sinushkin_alexey@mail.ru>
2018-12-18 00:26:18 +0800

    AddIn start

---
+++ Control/Excel2013Control/ToolBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace Excel2013Control
+{
+    public partial class ToolBox
+    {
+        private void ToolBox_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        void showMessage(string text)
+        {
+            System.Windows.Forms.MessageBox.Show(text);
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            showMessage("Click!");
+        }
+    }
+}
