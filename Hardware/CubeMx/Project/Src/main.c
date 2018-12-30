9034ce932a875f70c543c4f263a058423a363bd1
AlexeySinushkin <sinushkin_alexey@mail.ru>
2018-12-30 20:45:37 +0800

    при установке предделителя таймера 0 - делится на 1... ПИПЕЦ
    референс мануал, страничка 295

--- Hardware/CubeMx/Project/Src/main.c
+++ Hardware/CubeMx/Project/Src/main.c
@@ -212,7 +212,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 1;
+  htim1.Init.Prescaler = 0;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = TIMER_PERIOD;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -293,7 +293,7 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 1;
+  htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 60000;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -376,7 +376,7 @@ static void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 1;
+  htim3.Init.Prescaler = 0;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim3.Init.Period = GAP_PERIOD;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -448,7 +448,7 @@ static void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 1;
+  htim4.Init.Prescaler = 0;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim4.Init.Period = BUNCH;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
