9424542c5ab4ba7e5bf183a5b2fa9168d476676f
Alexey <sinushkin_alexey@mail.ru>
2019-03-28 22:45:07 +0800

    gitignore from
    https://github.com/github/gitignore/blob/master/VisualStudio.gitignore

--- Audio/Beating/Beating/Form1.Designer.cs
+++ Audio/Beating/Beating/Form1.Designer.cs
@@ -28,13 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.channel1 = new Beating.Channel();
+            this.channel2 = new Beating.Channel();
+            this.SuspendLayout();
+            // 
+            // channel1
+            // 
+            this.channel1.Location = new System.Drawing.Point(-23, -46);
+            this.channel1.Name = "channel1";
+            this.channel1.Size = new System.Drawing.Size(500, 181);
+            this.channel1.TabIndex = 0;
+            // 
+            // channel2
+            // 
+            this.channel2.Location = new System.Drawing.Point(12, 12);
+            this.channel2.Name = "channel2";
+            this.channel2.Size = new System.Drawing.Size(485, 44);
+            this.channel2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.channel2);
+            this.Controls.Add(this.channel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Channel channel1;
+        private Channel channel2;
     }
 }
 
