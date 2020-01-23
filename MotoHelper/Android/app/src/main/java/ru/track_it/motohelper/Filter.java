0f31f4b4987f7da4887a9020b7e7ace1eb072dd8
AlexeySinushkin <sinushkin_alexey@mail.ru>
2020-01-22 21:43:36 -0800

    Update Filter.java

--- MotoHelper/Android/app/src/main/java/ru/track_it/motohelper/Filter.java
+++ MotoHelper/Android/app/src/main/java/ru/track_it/motohelper/Filter.java
@@ -13,16 +13,37 @@ public class Filter {
 
     }
 
+    public static class AccelerationResult{
+        int value;
+        long lastMS;
+    }
 
     /**
      * @param data отсортированные данные
      */
-    void analizeData(AccelData[] data){
+    AccelerationResult analizeData(AccelData[] data, long previousTargetMs){
         if (data == null || data.length<minimumPointsForAnalize){
-            return;
+            return null;
         }
-        int firstIndexForAnalyze=0;
+        int previousTargetIndex = 0;
+        if (previousTargetMs!=0){
+            for (int i = 0; i<data.length;i++){
+                if (data[i].ms>=previousTargetMs)
+                {
+                    previousTargetIndex = i;
+                    break;
+                }
+            }
+        }
+
+        if (data.length-previousTargetIndex<minimumPointsForAnalize){
+            return null;
+        }
+
+        int sixth = minimumPointsForAnalize/6;
+        int firstIndexForAnalyze=data.length-sixth*3;
         int lastIndexForAnalyze = data.length-1;
+
         for (int i=lastIndexForAnalyze;i>=0;i--){
             AccelData accelData = data[i];
             if (accelData.ms<=lastAnalizedTimestamp){
@@ -36,23 +57,59 @@ public class Filter {
             return;
         }
 
-        int vSearchStart = lastIndexForAnalyze-(elementsToAnalyze/3);
-        int vSearchStop = lastIndexForAnalyze-(elementsToAnalyze/4);
-        
-        int sum = 0;
-        //в 25 процентной зоне спрва ищем точку тишины
-        for (int i=firstIndexForAnalyze;i<=lastIndexForAnalyze;i++){
-            sum+=data[i].x;
+        //min & max в первой трети анализируемой трети
+        int min1 = data[firstIndexForAnalyze].x;
+        int max1 = data[firstIndexForAnalyze].x;
+        int end = firstIndexForAnalyze + sixth;
+        for (int i = firstIndexForAnalyze; i<end;i++){
+            int x = data[i].x;
+            if (x<min1){
+                min1=x;
+            }
+            if (x>max1){
+                max1=x;
+            }
         }
-        sum/=elementsToAnalyze;
-        if (Math.abs(sum)<=beating){
-            // не произошло ничего
-            lastAnalizedTimestamp = data[data.length-1].ms;
-            return;
+        end = lastIndexForAnalyze - sixth;
+        int min3 = data[lastIndexForAnalyze].x;
+        int max3 = data[lastIndexForAnalyze].x;
+        for (int i = lastIndexForAnalyze; i>end;i--){
+            int x = data[i].x;
+            if (x<min3){
+                min3=x;
+            }
+            if (x>max3){
+                max3=x;
+            }
         }
-        //ищем точку в)
 
+        int avg1 = (max1-min1)/2;
+        int avg3 = (max3-min3)/2;
+        int avg2 = (avg3-avg1)/2;
 
+        //ищем точку ближайшую с реднему значению
+        int differ = Math.abs(data[lastIndexForAnalyze].x-avg2);
+        int targetIndex = lastIndexForAnalyze;
+        for (int i = lastIndexForAnalyze; i>firstIndexForAnalyze;i--){
+            int nextDiffer = Math.abs(data[i].x-avg2);
+            if (nextDiffer < differ){
+                differ = nextDiffer;
+                targetIndex = i;
+            }
+        }
+
+        //к этому моменту мы имеем искомый средний индекс
+        int avgX=0;
+        int count = targetIndex-previousTargetIndex+1;
+        for (int i = previousTargetIndex; i<=targetIndex;i++){
+            avgX+=data[i].x;
+        }
+        avgX/=count;
+
+        AccelerationResult result = new AccelerationResult();
+        result.value = avgX;
+        result.lastMS = data[targetIndex].ms;
+        return result;
     }
 
 
