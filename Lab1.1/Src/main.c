18bd50272e6da369748f8e927c322f6207136401
Alexey <sinushkin_alexey@mail.ru>
2021-03-14 20:13:37 +0800

    полярность

--- Lab1.1/Src/main.c
+++ Lab1.1/Src/main.c
@@ -319,6 +319,7 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
   {
     Error_Handler();
